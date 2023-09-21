c7b1c2a20778a77139d652a8857fe427dfd3262d
Martinho Prata <martinhoprata95@gmail.com>
2023-09-21 16:46:51 -0300

    Project Created

---
+++ src/main/java/com/pratatecm/controllingDebt/ControllingDebtApplication.java
@@ -0,0 +1,13 @@
+package com.pratatecm.controllingDebt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllingDebtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllingDebtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratatecm/controllingDebt/ControllingDebtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratatecm.controllingDebt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllingDebtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
