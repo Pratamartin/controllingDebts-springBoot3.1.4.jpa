d2efefadf5f1879c9b59c108508309859712960d
Martinho Prata <martinhoprata95@gmail.com>
2023-09-28 17:09:14 -0300

    H2 database, test profile, JPA

--- src/main/java/com/pratatecm/controllingDebt/entities/User.java
+++ src/main/java/com/pratatecm/controllingDebt/entities/User.java
@@ -1,23 +1,32 @@
 package com.pratatecm.controllingDebt.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
 
+    public User() {
+
+    }
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.phone = phone;
         this.password =password;
     }
-
     public Long getId() {
         return id;
     }
